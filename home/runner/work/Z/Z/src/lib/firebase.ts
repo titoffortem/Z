04f2b9de6b32dd686fa02dfef44b1a9981368b0f
Nikita Lebedev <46885334+nikgohn@users.noreply.github.com>
2026-02-16 22:47:59 +0300

    removed home folder

--- home/runner/work/Z/Z/src/lib/firebase.ts
+++
@@ -1,2 +0,0 @@
-// This file is intentionally left empty to prevent conflicting Firebase initializations.
-// All Firebase services should be accessed through the hooks provided in /src/firebase/index.ts.
